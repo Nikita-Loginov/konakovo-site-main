6f953a804afc2fda92dd5d1a5244c263efb40cb6
Nikita Loginov <nikitaloginov264@gmail.com>
2025-07-02 21:05:48 +0400

    url логика для табов

--- js/modules/tab.js
+++ js/modules/tab.js
@@ -12,8 +12,6 @@ export function searcBlockTab(e, functionTab) {
 
   if (!btn) return;
 
-
-
   const nameTab = btn.dataset.btnTab;
 
   const block = document.querySelector(`[data-block-tab="${nameTab}"]`);
@@ -21,17 +19,67 @@ export function searcBlockTab(e, functionTab) {
   if (block) {
     toggleClassesTabElements({ btn, block, nameTab, functionTab });
   }
-  
 }
 
 function toggleClassesTabElements(elements) {
-  const { btn, block ,nameTab, functionTab } = elements;
+  const { btn, block, nameTab, functionTab } = elements;
 
   if (block) {
     const tabsBox = btn.closest(".tabs-box");
     const tabsBoxName = tabsBox.dataset.tabCategory;
 
+    if (tabsBox.dataset.url) {
+      const name = tabsBox.dataset.url;
 
+      const url = new URL(window.location.href);
+
+      url.searchParams.set(name, nameTab);
+
+      history.pushState({}, "", url);
+
+      const searchs = url.search.slice(1).split("&");
+
+      if (searchs.length > 1) {
+        const currentSearchs = searchs.slice(0, searchs.length - 1);
+
+        currentSearchs.forEach((search, index) => {
+          const item = search.split("=");
+
+          const category = item[0];
+          const name = item[1];
+
+          const findActiveBlockItem = document.querySelector(
+            `[data-block-tab="${name}"]`
+          );
+
+          if (!category || !name || !findActiveBlockItem) return;
+
+          const btns = findActiveBlockItem.querySelector(".tabs__btns");
+
+          if (!btns) return;
+
+          const activeBtn = btns.querySelector(
+            `.${TAB__INFO.nameTabBtn}.${TAB__INFO.nameActiveClass}`
+          );
+
+          const firstBtn = btns.querySelector(
+            `.${TAB__INFO.nameTabBtn}`
+          )
+
+          const itemNeed = searchs[index + 1].split("=");
+
+          const categoryItem = itemNeed[0];
+          const nameItem = itemNeed[1];
+
+          if (activeBtn) {   
+            url.searchParams.set(categoryItem, nameItem);
+            history.pushState({}, "", url);
+          } else {
+            firstBtn.click()
+          }
+        });
+      }
+    }
 
     const prevActiveBtn = tabsBox.querySelector(
       `.${TAB__INFO.nameTabBtn}.${TAB__INFO.nameActiveClass}`
@@ -39,8 +87,6 @@ function toggleClassesTabElements(elements) {
     const prevActiveBlock = tabsBox.querySelector(
       `.${TAB__INFO.nameTabBlock}.${TAB__INFO.nameActiveClass}[data-block-category=${tabsBoxName}]`
     );
-
-    
 
     if (prevActiveBtn)
       classAction(prevActiveBtn, TAB__INFO.nameActiveClass, "remove");
@@ -50,6 +96,20 @@ function toggleClassesTabElements(elements) {
     if (btn.closest(".tabs__btns")) {
       classAction(btn, TAB__INFO.nameActiveClass, "add");
       classAction(block, TAB__INFO.nameActiveClass, "add");
+
+      const activeBtn = tabsBox.querySelector(
+        `.${TAB__INFO.nameTabBtn}.${TAB__INFO.nameActiveClass}.active`
+      );
+
+      // if (!activeBtn) {
+      //   const blockFind = block.querySelector(`.${TAB__INFO.nameTabBtn}`)
+      //   console.log(blockFind)
+      //   classAction(
+      //     blockFind,
+      //     TAB__INFO.nameActiveClass,
+      //     "add"
+      //   );
+      // }
 
       if (functionTab) {
         functionTab();
@@ -71,3 +131,29 @@ function toggleClassesTabElements(elements) {
     }
   }
 }
+
+export const setFirstActiveClasses = () => {
+  const tabsUrl = document.querySelectorAll(".tabs-url");
+
+  tabsUrl.forEach((tabBox) => {
+    const firstBtnsBox = tabBox.querySelector(".tabs__btns");
+
+    if (!firstBtnsBox) return;
+
+    const firstBtn = firstBtnsBox.querySelector(`.${TAB__INFO.nameTabBtn}`);
+
+    const nameBox = firstBtn.dataset.btnTab;
+
+    if (!nameBox) return;
+
+    firstBtn.click();
+
+    // console.log(nameBox)
+
+    const block = document.querySelector(`[data-block-tab="${nameBox}"]`);
+
+    const secondBtn = block.querySelector(`.${TAB__INFO.nameTabBtn}`);
+
+    secondBtn.click();
+  });
+};
--- js/price.js
+++ js/price.js
@@ -1,13 +1,14 @@
 import { searcBlockTab } from "./modules/tab.js";
 import { handleAllSliders, slidersConfig } from "./modules/swiper.js";
 import { toggleAccordeonItems } from "./modules/accordeon.js";
+import { setFirstActiveClasses } from "./modules/tab.js";
 
 const swipers = [
   {
     selector: ".price-sidebar.swiper",
     breakpoint: 901,
     options: {
-      slidesPerView: 'auto',
+      slidesPerView: "auto",
       spaceBetween: 12,
     },
   },
@@ -15,24 +16,72 @@ const swipers = [
     selector: ".price__mini-tabs.swiper",
     breakpoint: 7423423542567,
     options: {
-      slidesPerView: 'auto',
+      slidesPerView: "auto",
       spaceBetween: 20,
     },
   },
 ];
 
-document.addEventListener('click', (e) => {
-    searcBlockTab(e);
-    toggleAccordeonItems(e);;
-})
+document.addEventListener("click", (e) => {
+  searcBlockTab(e);
+  toggleAccordeonItems(e);
+});
 
-document.addEventListener('DOMContentLoaded', () => {
- swipers.forEach((config) => {
-    slidersConfig.push(config)
-  })
+document.addEventListener("DOMContentLoaded", () => {
+  swipers.forEach((config) => {
+    slidersConfig.push(config);
+  });
 
   handleAllSliders();
-})
+
+  setActiveClass();
+});
+
+const setActiveClass = () => {
+  const url = new URL(window.location.href);
+
+  if (url.searchParams.size) {
+    const searchs = url.search.slice(1).split("&");
+
+    if (searchs.length) {
+      for (let i = 0; i < searchs.length; i++) {
+        const item = searchs[i].split("=");
+
+        const category = item[0];
+        const name = item[1];
+
+        // console.log(category);
+
+        if (!category || !name) return;
+
+        // const findItemBlock = document.querySelector(
+        //   `[data-url="${category}"]`
+        // );
+
+        // if (!findItemBlock) return;
+
+        const findActiveBtnItem = document.querySelector(
+          `[data-url-block="${name}"]`
+        );
+
+        const findActiveBlockItem = document.querySelector(
+          `[data-block-tab="${name}"]`
+        );
+
+        // console.log(findItemBlock, `[data-block-tab="${name}"]`)
+
+        if (findActiveBtnItem && findActiveBtnItem.dataset.btnTab) {
+          findActiveBtnItem.classList.add("active");
+          findActiveBlockItem.classList.add("active");
+        }
+      }
+    }
+
+    return;
+  }
+
+  setFirstActiveClasses()
+};
 
 let resizeTimeout;
 window.addEventListener("resize", () => {
